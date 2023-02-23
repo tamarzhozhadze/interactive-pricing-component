1ac9a88de474b7c5ae8b9b9bbe5916493fba8cf7
zhozhadze-tamar <zhozhadzetamar@gmail.com>
2023-02-23 15:29:07 +0400

    JS finished

--- script.js
+++ script.js
@@ -1,32 +1,70 @@
 const buttonJS = document.querySelector(".turnOnButton");
 const money = document.querySelector(".price-text-style");
 const sliderJS = document.querySelector(".slider");
+const viewJS = document.querySelector(".pageViews-style");
+
+let  knobJS = true;
+
+
+function choose() {
+  buttonJS.classList.toggle("active");
+  knobJS =!knobJS;
+  console.log(knobJS);
+}
 
 
 sliderJS.addEventListener("input", function (event) {
   const percent = ((sliderJS.value - 1) / 4) * 100;
   sliderJS.style.backgroundSize = percent + "%";
+  insertValue(event.target.value);
 
-  if (event.target.value === "1") {
-    money.textContent = "$8.00";
-  }
-  if (event.target.value === "2") {
-    money.textContent = "$12.00";
-  }
-  if (event.target.value === "3") {
-    money.textContent = "$16.00";
-  }
-  if (event.target.value === "4") {
-    money.textContent = "$24.00";
-  }
-  if (event.target.value === "5") {
-    money.textContent = "$36.00";
-  }
- 
 });
 
-function choose() {
-   buttonJS.classList.toggle("active");
-  
- }
+function insertValue(one){
+  if (one === "1" && knobJS) {
+    money.textContent = "$8.00";
+    viewJS.textContent = "10K pageviews"; 
+    
+  }
+  if (one === "2" && knobJS) {
+    money.textContent = "$12.00";
+    viewJS.textContent = "50K pageviews"; 
+  }
+  if (one === "3" && knobJS) {
+    money.textContent = "$16.00";
+    viewJS.textContent = "100K pageviews"; 
+  }
+  if (one === "4" && knobJS) {
+    money.textContent = "$24.00";
+    viewJS.textContent = "500K pageviews"; 
+  }
+  if (one === "5" && knobJS) {
+    money.textContent = "$36.00";
+    viewJS.textContent = "1M pageviews"; 
+  }
+
+
+
+  if (one === "1" && !knobJS) {
+    money.textContent = "$12.00";
+    viewJS.textContent = "10K pageviews"; 
+    
+  }
+  if (one === "2" && !knobJS) {
+    money.textContent = "$36";
+    viewJS.textContent = "50K pageviews"; 
+  }
+  if (one === "3" && !knobJS) {
+    money.textContent = "$48";
+    viewJS.textContent = "100K pageviews"; 
+  }
+  if (one === "4" && !knobJS) {
+    money.textContent = "$72.00";
+    viewJS.textContent = "500K pageviews"; 
+  }
+  if (one === "5" && !knobJS) {
+    money.textContent = "$108";
+    viewJS.textContent = "1M pageviews"; 
+  }
+}
 
